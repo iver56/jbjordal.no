84cc60c9015134cc074b6b163e7652375a7979ac
iver56 <iver56@hotmail.com>
2015-06-09 01:02:32 +0200

    Implement hamburger menu for mobile devices. Compress images.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+﻿function toggleMobileMenu() {
+	var nav = document.getElementById('nav-mobile');
+	if (nav.style.display === 'block') {
+		nav.style.display = 'none';
+	} else {
+		nav.style.display = 'block';
+	}
+}
