0359b3c26c2fe85b7cecbdb5d62f1c6bcc8b241d
iver56 <iver56@hotmail.com>
2015-08-01 14:35:57 +0200

    Reformat code

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 ﻿function toggleMobileMenu() {
-	var nav = document.getElementById('nav-mobile');
-	if (nav.style.display === 'block') {
-		nav.style.display = 'none';
-	} else {
-		nav.style.display = 'block';
-	}
+  var nav = document.getElementById('nav-mobile');
+  if (nav.style.display === 'block') {
+    nav.style.display = 'none';
+  } else {
+    nav.style.display = 'block';
+  }
 }
 
 function navigateTo(url) {
-	if (url) {
-		window.location.href = url;
-	}
+  if (url) {
+    window.location.href = url;
+  }
 }
